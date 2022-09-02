6bcf74eac60606a90e4b846a999fa624c53e357f
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-09-02 20:43:47 -0300

    feature: NetworkAdapter, NetworkAdapterProtocol and NetworkAdapterError

---
+++ TheMovieApp/Source/Foundation/Adapter/NetworkAdapter.swift
@@ -0,0 +1,40 @@
+//
+//  NetworkAdapter.swift
+//  TheMovieApp
+//
+//  Created by Lucas Pinto on 02/09/22.
+//
+
+import Foundation
+
+protocol NetworkAdapterProtocol {
+    func parseRequest<T: Decodable>(networkRequest: NetworkRequest, completion: @escaping (Result<T, Error>) -> Void)
+}
+
+class NetwrokAdapter: NetworkAdapterProtocol {
+    var networkManager: NetworkManager
+    
+    init(networkManager: NetworkManager){
+        self.networkManager = networkManager
+    }
+    
+    func parseRequest<T: Decodable>(networkRequest: NetworkRequest ,completion: @escaping (Result<T, Error>) -> Void) {
+        
+        networkManager.getData(with: networkRequest) { (result) in
+            switch result {
+            case .success(let data):
+                do {
+                    let modelAdapted = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(modelAdapted))
+                } catch {
+                    print(error)
+                    completion(.failure(NetworkAdapterError.decodeError))
+                }
+            case .failure(_):
+                completion(.failure(NetworkAdapterError.noData))
+            }
+        }
+    }
+    
+    
+}
---
+++ TheMovieApp/Source/Foundation/Adapter/NetworkAdapterError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkAdapterError.swift
+//  TheMovieApp
+//
+//  Created by Lucas Pinto on 02/09/22.
+//
+
+import Foundation
+
+enum NetworkAdapterError: Error {
+    case noData
+    case decodeError
+}
